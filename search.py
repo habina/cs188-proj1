e68c6b656c5d3bab0d7bef9d5baf55d6944048c6
Kady <juns123@berkeley.edu>
2015-02-02 00:28:38 -0800

    Failed one test case for Q2, need Hunnie's help

--- search.py
+++ search.py
@@ -114,8 +114,31 @@ def depthFirstSearch(problem):
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
-
+    fringe = util.Queue()
+    fringe.push(problem.getStartState())
+    closed = set()
+    node_parent = {}
+    node_parent_direction = {}
+    def find_path(node):
+        path = []
+        while node in node_parent:
+            path.insert(0, node_parent_direction[node])
+            node = node_parent[node]
+        return path
+    while not fringe.isEmpty():
+        node = fringe.pop()
+        if problem.isGoalState(node):
+            print node_parent
+            print node_parent_direction
+            return find_path(node)
+        if node not in closed:
+            closed.add(node)
+            # print problem.getSuccessors(node)
+            for triple in problem.getSuccessors(node):
+                if triple[0] not in closed:
+                    fringe.push(triple[0])
+                    node_parent[triple[0]] = node
+                    node_parent_direction[triple[0]] = triple[1]
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
